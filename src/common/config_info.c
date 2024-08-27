bcbddd3ab0cf6abb293effe62a5d5fd756bd7bef
gorloffslava <gorloff.slava@gmail.com>
2024-08-27 16:38:30 +0500

    + Removed `HAVE_COMPUTED_GOTO`

--- src/common/config_info.c
+++ src/common/config_info.c
@@ -32,146 +32,159 @@
 ConfigData *
 get_configdata(const char *my_exec_path, size_t *configdata_len)
 {
-	ConfigData *configdata;
-	char		path[MAXPGPATH];
-	char	   *lastsep;
-	int			i = 0;
+    ConfigData *configdata;
+    char		path[MAXPGPATH];
+    char	   *lastsep;
+    int			i = 0;
 
-	/* Adjust this to match the number of items filled below */
-	*configdata_len = 19;
-	configdata = palloc_array(ConfigData, *configdata_len);
+    /* Adjust this to match the number of items filled below */
+    *configdata_len = 23;
+    configdata = palloc_array(ConfigData, *configdata_len);
 
-	configdata[i].name = pstrdup("BINDIR");
-	strlcpy(path, my_exec_path, sizeof(path));
-	lastsep = strrchr(path, '/');
-	if (lastsep)
-		*lastsep = '\0';
-	cleanup_path(path);
-	configdata[i].setting = pstrdup(path);
-	i++;
+    configdata[i].name = pstrdup("BINDIR");
+    strlcpy(path, my_exec_path, sizeof(path));
+    lastsep = strrchr(path, '/');
+    if (lastsep)
+        *lastsep = '\0';
+    cleanup_path(path);
+    configdata[i].setting = pstrdup(path);
+    i++;
 
-	configdata[i].name = pstrdup("INCLUDEDIR");
-	get_include_path(my_exec_path, path);
-	cleanup_path(path);
-	configdata[i].setting = pstrdup(path);
-	i++;
+    configdata[i].name = pstrdup("DOCDIR");
+    configdata[i].setting = pstrdup("<DOCDIR is unavailable in the libpq standalone build>");
+    i++;
 
-	configdata[i].name = pstrdup("PKGINCLUDEDIR");
-	get_pkginclude_path(my_exec_path, path);
-	cleanup_path(path);
-	configdata[i].setting = pstrdup(path);
-	i++;
+    configdata[i].name = pstrdup("HTMLDIR");
+    configdata[i].setting = pstrdup("<HTMLDIR is unavailable in the libpq standalone build>");
+    i++;
 
-	configdata[i].name = pstrdup("LIBDIR");
-	get_lib_path(my_exec_path, path);
-	cleanup_path(path);
-	configdata[i].setting = pstrdup(path);
-	i++;
+    configdata[i].name = pstrdup("INCLUDEDIR");
+    get_include_path(my_exec_path, path);
+    cleanup_path(path);
+    configdata[i].setting = pstrdup(path);
+    i++;
 
-	configdata[i].name = pstrdup("PKGLIBDIR");
-	get_pkglib_path(my_exec_path, path);
-	cleanup_path(path);
-	configdata[i].setting = pstrdup(path);
-	i++;
+    configdata[i].name = pstrdup("PKGINCLUDEDIR");
+    get_pkginclude_path(my_exec_path, path);
+    cleanup_path(path);
+    configdata[i].setting = pstrdup(path);
+    i++;
 
-	configdata[i].name = pstrdup("LOCALEDIR");
-	get_locale_path(my_exec_path, path);
-	cleanup_path(path);
-	configdata[i].setting = pstrdup(path);
-	i++;
+    configdata[i].name = pstrdup("INCLUDEDIR-SERVER");
+    configdata[i].setting = pstrdup("<INCLUDEDIR-SERVER is unavailable in the libpq standalone build>");
+    i++;
 
-	configdata[i].name = pstrdup("SHAREDIR");
-	get_share_path(my_exec_path, path);
-	cleanup_path(path);
-	configdata[i].setting = pstrdup(path);
-	i++;
+    configdata[i].name = pstrdup("LIBDIR");
+    get_lib_path(my_exec_path, path);
+    cleanup_path(path);
+    configdata[i].setting = pstrdup(path);
+    i++;
 
-	configdata[i].name = pstrdup("SYSCONFDIR");
-	get_etc_path(my_exec_path, path);
-	cleanup_path(path);
-	configdata[i].setting = pstrdup(path);
-	i++;
+    configdata[i].name = pstrdup("PKGLIBDIR");
+    get_pkglib_path(my_exec_path, path);
+    cleanup_path(path);
+    configdata[i].setting = pstrdup(path);
+    i++;
 
-	configdata[i].name = pstrdup("PGXS");
-	get_pkglib_path(my_exec_path, path);
-	strlcat(path, "/pgxs/src/makefiles/pgxs.mk", sizeof(path));
-	cleanup_path(path);
-	configdata[i].setting = pstrdup(path);
-	i++;
+    configdata[i].name = pstrdup("LOCALEDIR");
+    get_locale_path(my_exec_path, path);
+    cleanup_path(path);
+    configdata[i].setting = pstrdup(path);
+    i++;
 
-	configdata[i].name = pstrdup("CONFIGURE");
-	configdata[i].setting = pstrdup(CONFIGURE_ARGS);
-	i++;
+    configdata[i].name = pstrdup("MANDIR");
+    configdata[i].setting = pstrdup("<MANDIR is unavailable in the libpq standalone build>");
+    i++;
 
-	configdata[i].name = pstrdup("CC");
+    configdata[i].name = pstrdup("SHAREDIR");
+    get_share_path(my_exec_path, path);
+    cleanup_path(path);
+    configdata[i].setting = pstrdup(path);
+    i++;
+
+    configdata[i].name = pstrdup("SYSCONFDIR");
+    get_etc_path(my_exec_path, path);
+    cleanup_path(path);
+    configdata[i].setting = pstrdup(path);
+    i++;
+
+    configdata[i].name = pstrdup("PGXS");
+    configdata[i].setting = pstrdup("<PGXS is unavailable in the libpq standalone build>");
+    i++;
+
+    configdata[i].name = pstrdup("CONFIGURE");
+    configdata[i].setting = pstrdup(CONFIGURE_ARGS);
+    i++;
+
+    configdata[i].name = pstrdup("CC");
 #ifdef VAL_CC
-	configdata[i].setting = pstrdup(VAL_CC);
+    configdata[i].setting = pstrdup(VAL_CC);
 #else
-	configdata[i].setting = pstrdup(_("not recorded"));
+    configdata[i].setting = pstrdup(_("not recorded"));
 #endif
-	i++;
+    i++;
 
-	configdata[i].name = pstrdup("CPPFLAGS");
+    configdata[i].name = pstrdup("CPPFLAGS");
 #ifdef VAL_CPPFLAGS
-	configdata[i].setting = pstrdup(VAL_CPPFLAGS);
+    configdata[i].setting = pstrdup(VAL_CPPFLAGS);
 #else
-	configdata[i].setting = pstrdup(_("not recorded"));
+    configdata[i].setting = pstrdup(_("not recorded"));
 #endif
-	i++;
+    i++;
 
-	configdata[i].name = pstrdup("CFLAGS");
+    configdata[i].name = pstrdup("CFLAGS");
 #ifdef VAL_CFLAGS
-	configdata[i].setting = pstrdup(VAL_CFLAGS);
+    configdata[i].setting = pstrdup(VAL_CFLAGS);
 #else
-	configdata[i].setting = pstrdup(_("not recorded"));
+    configdata[i].setting = pstrdup(_("not recorded"));
 #endif
-	i++;
+    i++;
 
-	configdata[i].name = pstrdup("CFLAGS_SL");
+    configdata[i].name = pstrdup("CFLAGS_SL");
 #ifdef VAL_CFLAGS_SL
-	configdata[i].setting = pstrdup(VAL_CFLAGS_SL);
+    configdata[i].setting = pstrdup(VAL_CFLAGS_SL);
 #else
-	configdata[i].setting = pstrdup(_("not recorded"));
+    configdata[i].setting = pstrdup(_("not recorded"));
 #endif
-	i++;
+    i++;
 
-	configdata[i].name = pstrdup("LDFLAGS");
+    configdata[i].name = pstrdup("LDFLAGS");
 #ifdef VAL_LDFLAGS
-	configdata[i].setting = pstrdup(VAL_LDFLAGS);
+    configdata[i].setting = pstrdup(VAL_LDFLAGS);
 #else
-	configdata[i].setting = pstrdup(_("not recorded"));
+    configdata[i].setting = pstrdup(_("not recorded"));
 #endif
-	i++;
+    i++;
 
-	configdata[i].name = pstrdup("LDFLAGS_EX");
+    configdata[i].name = pstrdup("LDFLAGS_EX");
 #ifdef VAL_LDFLAGS_EX
-	configdata[i].setting = pstrdup(VAL_LDFLAGS_EX);
+    configdata[i].setting = pstrdup(VAL_LDFLAGS_EX);
 #else
-	configdata[i].setting = pstrdup(_("not recorded"));
+    configdata[i].setting = pstrdup(_("not recorded"));
 #endif
-	i++;
+    i++;
 
-	configdata[i].name = pstrdup("LDFLAGS_SL");
+    configdata[i].name = pstrdup("LDFLAGS_SL");
 #ifdef VAL_LDFLAGS_SL
-	configdata[i].setting = pstrdup(VAL_LDFLAGS_SL);
+    configdata[i].setting = pstrdup(VAL_LDFLAGS_SL);
 #else
-	configdata[i].setting = pstrdup(_("not recorded"));
+    configdata[i].setting = pstrdup(_("not recorded"));
 #endif
-	i++;
+    i++;
 
-	configdata[i].name = pstrdup("LIBS");
+    configdata[i].name = pstrdup("LIBS");
 #ifdef VAL_LIBS
-	configdata[i].setting = pstrdup(VAL_LIBS);
+    configdata[i].setting = pstrdup(VAL_LIBS);
 #else
-	configdata[i].setting = pstrdup(_("not recorded"));
+    configdata[i].setting = pstrdup(_("not recorded"));
 #endif
-	i++;
+    i++;
 
-	configdata[i].name = pstrdup("VERSION");
-	configdata[i].setting = pstrdup("PostgreSQL " PG_VERSION);
-	i++;
+    configdata[i].name = pstrdup("VERSION");
+    configdata[i].setting = pstrdup("PostgreSQL " PG_VERSION);
+    i++;
 
-	Assert(i == *configdata_len);
+    Assert(i == *configdata_len);
 
-	return configdata;
+    return configdata;
 }
